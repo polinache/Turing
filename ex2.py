7e6b436f89269c5e58b6107a212fb6309f3fb52f
polinache <polinachekalkina@gmail.com>
2016-05-16 12:35:41 +0400

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,45 @@
+q1  a -> a q3 R
+q1  b -> b q4 R
+q1  c -> c q5 R
+q1  d -> d q6 R
+q3  a -> a q3 R
+q3  b -> b q3 R
+q3  c -> c q3 R
+q3  d -> d q3 R
+q4  a -> a q4 R
+q4  b -> b q4 R
+q4  c -> c q4 R
+q4  d -> d q4 R
+q5  a -> a q5 R
+q5  b -> b q5 R
+q5  c -> c q5 R
+q5  d -> d q5 R
+q6  a -> a q6 R
+q6  b -> b q6 R
+q6  c -> c q6 R
+q6  d -> d q6 R
+q3  B -> B q7 L
+q4  B -> B q8 L
+q5  B -> B q9 L
+q6  B -> B q10 L
+q7  a -> B q2 L
+q7  b -> b STOP
+q7  c -> c STOP
+q7  d -> d STOP
+q8  a -> a STOP
+q8  b -> B q2 L
+q8  c -> c STOP
+q8  d -> d STOP
+q9  a -> a STOP
+q9  b -> b STOP
+q9  c -> B q2 L
+q9  d -> d STOP
+q10 a -> a STOP
+q10 b -> b STOP
+q10 c -> c STOP
+q10 d -> d q2 L
+q2  a -> B q2 L
+q2  b -> B q2 L
+q2  c -> B q2 L
+q2  d -> B q2 L
+q2  B -> B STOP
